70f45b39235d191bc6da34a3aa5d7ffdda57972a
Koppari <robin.stachon@helsinki.fi>
2018-02-16 06:31:22 +0200

    4.5 tehty

--- Osa 4/back/utils/list_helper.js
+++ Osa 4/back/utils/list_helper.js
@@ -8,6 +8,21 @@ const totalLikes = (blogs) => {
     return likes
 }
 
+const favoriteBlog = (blogs) => {
+    //maps blog.likes, then finds index of max value in another map/reduce
+    let fav = blogs
+        .map(blog => {
+        return blog.likes
+    })
+        .indexOf(blogs.map(blog => {
+            return blog.likes
+        }).reduce(function (sum, likes) {
+            return Math.max(sum, likes)
+        }, 0))
+    return blogs[fav]
+}
+
 module.exports = {
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }

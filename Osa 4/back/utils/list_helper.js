800f0d4eeb7d538a55174b2604806503bdc0914a
Koppari <robin.stachon@helsinki.fi>
2018-02-16 04:47:32 +0200

    4.4 tehty, dummy poistettu

--- Osa 4/back/utils/list_helper.js
+++ Osa 4/back/utils/list_helper.js
@@ -1,7 +1,13 @@
-const dummy = (blogs) => {
-    return 1   
+const totalLikes = (blogs) => {
+    let likes = blogs.map(blog => {
+        return blog.likes
+    })
+        .reduce(function (sum, likes) {
+            return sum + likes
+        }, 0)
+    return likes
 }
 
 module.exports = {
-    dummy
+    totalLikes
 }

99d482b211b835e81e9f474966ac1193136b6aa0
Koppari <robin.stachon@helsinki.fi>
2018-03-02 00:24:59 +0200

    5.13 asti

--- Osa 4/front/src/components/SimpleBlog.test.js
+++ Osa 4/front/src/components/SimpleBlog.test.js
@@ -3,17 +3,32 @@ import {shallow} from 'enzyme'
 import SimpleBlog from "../components/SimpleBlog"
 
 describe.only('<SimpleBlog />', () => {
-  it('renders title, author and likes', () => {
-    const blog = {
-        title: "test",
-        author: "test",
-        likes: 2
-    }
+    it('renders title, author and likes', () => {
+        const blog = {
+            title: "test",
+            author: "test",
+            likes: 2
+        }
 
-    const simpleBlogComponent = shallow(<SimpleBlog blog={blog}/>)
-    const contentDiv = simpleBlogComponent.find('.content')
+        const simpleBlogComponent = shallow(<SimpleBlog blog={blog}/>)
+        const contentDiv = simpleBlogComponent.find('.content')
 
-    console.log(blog.title, blog.author);
-    expect(contentDiv.text()).toContain(blog.title, blog.author, blog.likes)
-  })
+        expect(contentDiv.text()).toContain(blog.title, blog.author, blog.likes)
+    })
+
+    it('clicking like twice calls onClick function twice', () => {
+        const blog = {
+            title: "test",
+            author: "test",
+            likes: 0
+        }
+
+        const mockHandler = jest.fn()
+        const simpleBlogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler}/>)
+
+        const button = simpleBlogComponent.find('button')
+        button.simulate('click')
+
+        expect(mockHandler.mock.calls.length).toBe(1)
+    })
 })
